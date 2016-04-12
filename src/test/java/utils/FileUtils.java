2e2d706e559b2bb72218281826aeac48f669e305
Dc <dcduba@users.noreply.github.com>
2016-04-12 10:24:34 +0200

    Windows error fix
    Fixes a compilation error on Windows, where for some reason all absolute
    paths are prefixed with a /.

--- src/test/java/utils/FileUtils.java
+++ src/test/java/utils/FileUtils.java
@@ -3,6 +3,16 @@ package utils;
 public class FileUtils {
 
     public static String fileLocation(String path) {
-        return FileUtils.class.getResource(path).getPath();
+        String newpath = FileUtils.class.getResource(path).getPath();
+        String os = System.getProperty("os.name");
+        
+        /* This is a quick and dirty fix, as I am not sure what is causing this. For some reason a / is
+         * prepended to a full path (e.g. "/C:/path/to/file.txt"), causing it to fail. This dirty fix
+         * will remove this slash.
+         */
+        if(os.startsWith("Windows") && newpath.startsWith("/")) {
+        	newpath = newpath.substring(1);
+        }
+        return newpath;
     }
 }
